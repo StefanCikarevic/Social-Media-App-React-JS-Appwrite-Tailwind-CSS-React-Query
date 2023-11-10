8c779cc464cc2149bf94eb32f607745076b7adc0
StefanCikarevic <stefan.cikarevic@prodyna.com>
2023-11-10 14:29:14 +0100

    Init Project

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+import { Route, Routes } from "react-router-dom";
+
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout.tsx";
+import SignInForm from "./_auth/forms/SigninForm.tsx";
+import SignupForm from "./_auth/forms/SignupForm.tsx";
+import RootLayout from "./_root/RootLayout.tsx";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          {/*  public routes */}
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+        <Route element={<RootLayout />}>
+          {/*  private routes */}
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;

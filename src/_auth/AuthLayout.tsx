78e11948f84208ab7fbaa94761ad9adeb83556e8
StefanCikarevic <stefan.cikarevic@prodyna.com>
2023-11-15 11:45:17 +0100

    Auth Functionality - Appwrite

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,28 @@
+import { Navigate, Outlet } from "react-router-dom";
+
 const AuthLayout = () => {
-  return <div></div>;
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col">
+            <Outlet />
+          </section>
+          <img
+            src="/assets/images/side-img.svg"
+            alt="block"
+            className={
+              "hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+            }
+          />
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,10 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(2, { message: "To short" }),
+  username: z.string().min(2),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 8 characters" }),
+});
